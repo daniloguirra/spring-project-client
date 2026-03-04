149dad5647560b5a00cdc9d965f42a038ee82789
daniloguirra <daniloguirra@hotmail.com>
2026-03-04 09:53:44 -0300

    controllers

---
+++ src/main/java/com/danilodev/desafiocrud/controllers/ClientController.java
@@ -0,0 +1,20 @@
+package com.danilodev.desafiocrud.controllers;
+
+import com.danilodev.desafiocrud.dto.ClientDTO;
+import com.danilodev.desafiocrud.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    
+
+}
